531b98434ab8e73b4de3afcc1a06e5bf08e75479
Artem Mindrov <artem.mindrov@gmail.com>
2016-03-29 19:06:10 +0300

    Skeleton rails-api app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Aureso::Application.config.secret_token = '28380b986e03beee5c32e553a6da349e1a20ce1ef7963fcae4ba3c1c8fe12d79223b72b77378dd9191503a6145531709ac711aa513533129b043696d86798864'
