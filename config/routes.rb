7b54e916052928f3f9ea0600827b5dc17232d67b
Artem Mindrov <artem.mindrov@gmail.com>
2016-04-02 19:28:45 +0300

    Second route for the public API

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
       resources :models, only: [] do
         resources :model_types, only: :index
+        post "model_types_price/:model_type_id", to: "model_types#model_types_price"
       end
     end
   end
--- spec/routing/model_types_routing_spec.rb
+++ spec/routing/model_types_routing_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 describe Api::V1::ModelTypesController, type: :routing do
   it { expect(get: "/api/models/model-slug/model_types").to route_to("api/v1/model_types#index",
                                                                      model_id: "model-slug", format: "json") }
+  it { expect(post: "/api/models/model-slug/model_types_price/model-type-slug").to(
+      route_to("api/v1/model_types#model_types_price",
+               format: "json", model_id: "model-slug", model_type_id: "model-type-slug")) }
 end
