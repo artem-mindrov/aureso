efa210b02a0f9646d7f63706b1bfd75c1bc2fc0f
Artem Mindrov <artem.mindrov@gmail.com>
2016-04-03 11:44:53 +0300

    Some more db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,16 @@
 end
 
 org = Organization.find_by_public_name("BMW")
+models = (1..7).map { |i| { name: "Series #{i}" } }
+org.models.create(models)
 
-1.upto(7) do |i|
-  org.models.create(name: "Series #{i}")
-end
+org.models.find("series-1").model_types.create(name: "M135i", base_price: 25000)
+org.models.find("series-2").model_types.create([
+                                                   { name: "Coupe", base_price: 28000 },
+                                                   { name: "Convertible", base_price: 32000 },
+                                                   { name: "Active Tourer", base_price: 36000 },
+                                                   { name: "Gran Tourer", base_price: 40000 },
+                                               ])
 
 org = Organization.find_by_public_name("Audi")
 
@@ -26,4 +32,15 @@ org = Organization.find_by_public_name("Audi")
   org.models.create(name: series)
 end
 
-Organization.find_by_public_name("Volkswagen").models.create(name: "Golf")
+model_types = (1..8).map { |i| { name: "Series #{i}", base_price: 28000 + i * 4000 } }
+org.models.find("a").model_types.create(model_types)
+
+model = Organization.find_by_public_name("Volkswagen").models.create(name: "Golf")
+model.model_types.create([
+                             { name: "Golf", base_price: "20000" },
+                             { name: "GTI", base_price: "25000" },
+                             { name: "R", base_price: "35000" },
+                             { name: "e-Golf", base_price: 29000 }
+                         ])
+
+User.create(email: "user@aureso.com", password: "password")
